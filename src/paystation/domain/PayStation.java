2cf4a510357972102ab4090e2ff01b3da8778f8c
Victor Dang <victordang15@gmail.com>
2018-09-22 14:20:13 -0400

    Changed cancel method and added empty method

--- src/paystation/domain/PayStation.java
+++ src/paystation/domain/PayStation.java
@@ -17,7 +17,10 @@
  * implied. You may study, use, modify, and distribute it for non-commercial
  * purposes. For any commercial use, see http://www.baerbak.com/
  */
+
 package paystation.domain;
+import java.util.*;
+
 
 public interface PayStation {
 
@@ -49,5 +52,12 @@ public interface PayStation {
     /**
      * Cancel the present transaction. Resets the machine for a new transaction.
      */
-    public void cancel();
-}
+    public Map<Integer, Integer> cancel();
+   
+    
+    
+    public int empty();
+
+
+
+}//end of paystation
